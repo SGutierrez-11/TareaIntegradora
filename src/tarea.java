1e7ac00cc5461915f795be6708a447e239e434b0
Santiago Gutierrez <santicoguvi02@gmail.com>
2020-09-19 16:14:19 -0500

    Ya acabe el proyecto, y sin embargo para la segunda entrega sería hacerlo en función de métodos.

--- src/tarea.java
+++ src/tarea.java
@@ -3,35 +3,29 @@ public class tarea{
 	
 	public static Scanner lector = new Scanner (System.in);
 	
-		public static void main (String[] args){
+	/**
+	*createMaterial This method is used for the user to enter the name of each material, quantity and type of use.
+	*<b> pre: </b>: 
+	*<b> pos: </b>:
+	*@param material Name of de materials
+	*@param	amountProduct Amount of each material
+	*@param work Type of use
+	*@return output, is a array.
+	*/
+	
+	public static int [] createMaterials(String[] material, 
+	double[] amountProduct,Utility[] work){
 			
-			int blackW,whiteW,paintW; // Estos son contadores que usaré para definir si hay un tipo de obra o pintura dentro del primer for().
+			int [] output = new int[3]; //This array have the role to save the information in blackW, whiteW and paintW.
 			
-			double totalWork; // Esta variable funciona en funcion de la sumatoria de las constantes WORKBLACK,WORKWHITE y PAINTING (Dependiendo de la analogía a la tabla de verdad).
-			
-			System.out.println("Ingrese la cantidad de materiales asignados (cantidad por tipo): ");
+		 int blackW, whiteW, paintW; //These variables have the role of storing the sum of the enum Utility.
+
+		 
+		 blackW=0;
+		 whiteW=0;
+		 paintW=0;
 		
-			int totalMaterial = lector.nextInt(); // 
-			
-			String [] material = new String[totalMaterial]; 
-			
-			double [] priceHomecenter = new double[totalMaterial];
-			
-			double [] priceFcentro = new double[totalMaterial];
-			
-			double [] priceFbarrio = new double[totalMaterial];
-			
-			double [] amountProduct = new double[totalMaterial]; // La cantidad de cada producto (ejemplo: 10 ladrillos)
-			 
-			Utility [] work = new Utility[totalMaterial]; // Declaramos la variable work como arreglo con el enum utility.
-			
-			Ubication placeUbi; // Declaramos la variable placeUbi (lugar de ubicacion para el domicilio) con enum ubication.
-			
-			System.out.println("Ingrese el nombre de cada material (por tipo): ");
-			
-			blackW=whiteW=paintW=0;
-			
-				for (int i=0;i<totalMaterial;i++){      // Este FOR se usa para ingresa el nombre de materia, cantidad y tipo de utilizacion.
+		for (int i=0;i<material.length;i++){      
 					
 					lector.nextLine();
 					
@@ -51,22 +45,63 @@ public class tarea{
 					switch (joinUtility){
 						case 1:
 							work[i]=Utility.OBRANEGRA;
+							blackW++;
+							break;
 						case 2:
 							work[i]=Utility.OBRABLANCA;
+							whiteW++;
+							break;
 						case 3:
 							work[i]=Utility.PINTURA;
-					}
-					
-					if (work[i]==Utility.OBRANEGRA){
-						blackW++;
-					}if (work[i]==Utility.OBRABLANCA){
-						whiteW++;
-					}if (work[i]==Utility.PINTURA){
-						paintW++;
+							paintW++;
+							break;
 					}
 				}
-						
-					totalWork=0;														 //TABLA DE VERDAD:
+			output[0]=blackW;
+			output[1]=whiteW;
+			output[2]=paintW;
+			return output;
+	}
+	
+		/**
+	*main This method is used for the user enter different data for the program run. 
+	*<b> pre: </b>:  
+	*<b> pos: </b>:
+	*/
+	
+		public static void main (String[] args){                                 // MAIN METHOD 
+			
+			int blackW,whiteW,paintW;
+			
+			double totalWork; // This variable works based on the enum Utility, and would refer to the truth table.
+			
+			System.out.println("Ingrese la cantidad de materiales asignados (cantidad por tipo): ");
+		
+			int totalMaterial = lector.nextInt(); // This lector is to receive the total of materials.
+			
+			String [] material = new String[totalMaterial];  // Array to save the names of materials.
+			
+			double [] priceHomecenter = new double[totalMaterial]; //Array to save the price of the materials in Homecenter.
+			
+			double [] priceFcentro = new double[totalMaterial]; //Array to save the price of the materials in Ferretería del Centro.
+			
+			double [] priceFbarrio = new double[totalMaterial]; //Array to save the price of the materials in Ferreteria del Barrio.
+			
+			double [] amountProduct = new double[totalMaterial]; // The quantity of each product (example: 10 bricks).
+			 
+			Utility [] work = new Utility[totalMaterial]; // Declare the work variable as an array with the enum Utility.
+			
+			Ubication placeUbi; // Declare the variable placeUbi (place of location for the address) with enum Ubication.
+			
+			System.out.println("Ingrese el nombre de cada material (por tipo): ");
+			
+			int [] workHand = createMaterials(material, amountProduct, work);               // CALL TO METHOD.
+			
+			blackW=workHand[0];
+			whiteW=workHand[1];
+			paintW=workHand[2];
+				
+					totalWork=0;														 //Truth table analogy
 																										
 				if (blackW>0 && whiteW>0 && paintW>0){     // V - V - V 
 					totalWork=4880000;  
@@ -88,10 +123,10 @@ public class tarea{
 				
 			System.out.println("Primero, digite los valores de compra por unidad de los materiales en HOMECENTER: ");
 				
-				double hcPayment=0;
-				double multiHC=0;
+				double hcPayment=0;  // This variable have the role of save the total to pay at Homecenter.
+				double multiHC=0; //This variable is only to save a multiplication between the arrays of prices and amount of each material.
 				
-				for (int i=0;i<totalMaterial;i++){                                          //PRECIO MATERIAL HOMECENTER
+				for (int i=0;i<totalMaterial;i++){                                          //PRICE OF MATERIALS IN HOMECENTER
 					
 					System.out.println("Precio del material ("+material[i]+"):");
 					
@@ -105,10 +140,10 @@ public class tarea{
 			
 			System.out.println("Segundo, digite los valores de compra por unidad de los materiales en FERRETERIA DEL CENTRO");
 			
-				double fcPayment=0;
-				double multiFC=0;
+				double fcPayment=0; // This variable have the role of save the total to pay at Ferreteria del centro.
+				double multiFC=0;  //This variable is only to save a multiplication between the arrays of prices and amount of each material.
 			
-				for (int i=0;i<totalMaterial;i++){                                          //PRECIO MATERIAL FERRETERIA DEL CENTRO
+				for (int i=0;i<totalMaterial;i++){                                          //PRICE OF MATERIALS IN FERRETERIA DEL CENTRO
 					
 					System.out.println("Precio del material ("+material[i]+"):");
 					
@@ -122,9 +157,10 @@ public class tarea{
 				
 			System.out.println("Tercero, digite los valores de compra por unidad de los materiales en FERRETERIA DEL BARRIO");
 			
-				double fbPayment=0;
-				double multiFB=0;
-				for (int i=0;i<totalMaterial;i++){                                         //PRECIO MATERIAL FERRETERIA DEL BARRIO
+				double fbPayment=0; // This variable have the role of save the total to pay at Ferreteria del barrio.
+				double multiFB=0;  //This variable is only to save a multiplication between the arrays of prices and amount of each material.
+				
+				for (int i=0;i<totalMaterial;i++){                                         //PRICE OF MATERIALS IN FERRETERIA DEL BARRIO
 					
 					System.out.println("Precio del material ("+material[i]+"):");
 					
@@ -144,7 +180,7 @@ public class tarea{
 					
 					System.out.println("Defina la ubicacion de entrega (1. Norte /2. Centro /3. Sur): ");
 					
-					int joinUbication=lector.nextInt();
+					int joinUbication=lector.nextInt();  // 
 					
 					placeUbi=null;
 					
@@ -160,9 +196,9 @@ public class tarea{
 								break;
 					}
 					
-					if (placeUbi==Ubication.NORTE){              // DOMICILIO EN EL NORTE      <--------------------------------NORTE
+					if (placeUbi==Ubication.NORTE){              // DELIVERY AT NORTH     <--------------------------------NORTH
 						
-						if(hcPayment>300000){                                                  // TOTAL COMPRA HOMECENTER
+						if(hcPayment>300000){                                                  // TOTAL PURCHASE HOMECENTER
 				
 								System.out.println("El total a pagar en Homecenter es: "+(hcPayment+totalWork));
 						
@@ -176,7 +212,7 @@ public class tarea{
 									
 								}
 								
-							if(fcPayment>300000){                                                  //TOTAL COMPRA FERRETERIA DEL CENTRO
+							if(fcPayment>300000){                                                  //TOTAL PURCHASE FERRETERIA DEL CENTRO
 				
 								System.out.println("El total a pagar en ferreteria del centro es: "+(fcPayment+totalWork));
 						
@@ -190,7 +226,7 @@ public class tarea{
 									
 								}
 								
-							if(fbPayment>300000){                                                 //TOTAL COMPRA FERRETERIA DEL BARRIO
+							if(fbPayment>300000){                                                 //TOTAL PURCHASE FERRETERIA DEL BARRIO
 				
 								System.out.println("El total a pagar en ferreteria del barrio es: "+(fbPayment+totalWork));
 						
@@ -205,9 +241,9 @@ public class tarea{
 								}
 					
 						}
-						if(placeUbi==Ubication.CENTRO){                     // DOMICILIO EN EL CENTRO     <------------------------CENTRO
+						if(placeUbi==Ubication.CENTRO){                     // DELIVERY AT CENTER     <------------------------CENTER
 							
-							if(hcPayment>300000){                                               // TOTAL COMPRA HOMECENTER
+							if(hcPayment>300000){                                               // TOTAL PURCHASE HOMECENTER
 				
 								System.out.println("El total a pagar en Homecenter es: "+(hcPayment+totalWork));
 						
@@ -221,7 +257,7 @@ public class tarea{
 									
 								}
 								
-							if(fcPayment>300000){                                              //TOTAL COMPRA FERRETERIA DEL CENTRO
+							if(fcPayment>300000){                                              //TOTAL PURCHASE FERRETERIA DEL CENTRO
 				
 								System.out.println("El total a pagar en ferreteria del centro es: "+(fcPayment+totalWork));
 						
@@ -235,7 +271,7 @@ public class tarea{
 									
 								}
 								
-							if(fbPayment>300000){                                             //TOTAL COMPRA FERRETERIA DEL BARRIO
+							if(fbPayment>300000){                                             //TOTAL PURCHASE FERRETERIA DEL BARRIO
 				
 								System.out.println("El total a pagar en ferreteria del barrio es: "+(fbPayment+totalWork));
 						
@@ -250,9 +286,9 @@ public class tarea{
 								}
 							
 						}
-						if(placeUbi==Ubication.SUR){                        // DOMICILIO EN EL SUR   <---------------------------------SUR
+						if(placeUbi==Ubication.SUR){                        // DELIVERY AT SOUTH   <---------------------------------SOUTH
 							
-							if(hcPayment>300000){    // TOTAL COMPRA HOMECENTER
+							if(hcPayment>300000){    // TOTAL PURCHASE HOMECENTER
 				
 								System.out.println("El total a pagar en Homecenter es: "+(hcPayment+totalWork));
 						
@@ -266,7 +302,7 @@ public class tarea{
 									
 								}
 								
-							if(fcPayment>300000){     //TOTAL COMPRA FERRETERIA DEL CENTRO
+							if(fcPayment>300000){     //TOTAL PURCHASE FERRETERIA DEL CENTRO
 				
 								System.out.println("El total a pagar en ferreteria del centro es: "+(fcPayment+totalWork));
 						
@@ -280,7 +316,7 @@ public class tarea{
 									
 								}
 								
-							if(fbPayment>300000){    //TOTAL COMPRA FERRETERIA DEL BARRIO
+							if(fbPayment>300000){    //TOTAL PURCHASE FERRETERIA DEL BARRIO
 				
 								System.out.println("El total a pagar en ferreteria del barrio es: "+(fbPayment+totalWork));
 						
@@ -296,15 +332,45 @@ public class tarea{
 							
 						}
 						
-				}else{
+				}else{                                                                              // NOT DELIVERY
 					System.out.println("El total a pagar en Homecenter es: "+(hcPayment+totalWork));
 					System.out.println("El total a pagar en ferreteria del barrio es: "+(fcPayment+totalWork));
 					System.out.println("El total a pagar en ferreteria del barrio es: "+(fbPayment+totalWork));
 				}
 				
-				double [] bestPrice= new double[totalMaterial];
+				double [] bestPrice= new double[totalMaterial]; // Array to save cheap prices between Homecenter, ferreteria del centro y ferreteria del barrio
 				
+				String [] bestStore=new String[totalMaterial]; //Array to save the store with the best price.
 				
+				for (int i=0;i<totalMaterial;i++){
+					
+					if(priceHomecenter[i]<priceFcentro[i] && priceHomecenter[i]<priceFbarrio[i]){
+						
+						bestPrice[i]=priceHomecenter[i];
+						bestStore[i]="Homecenter";
+						
+				}if(priceFcentro[i]<priceHomecenter[i] && priceFcentro[i]<priceFbarrio[i]){
+						bestPrice[i]=priceFcentro[i];
+						bestStore[i]="Ferreteria del centro";
+					}if(priceFbarrio[i]<priceHomecenter[i] && priceFbarrio[i]<priceFcentro[i]){
+						bestPrice[i]=priceFbarrio[i];
+						bestStore[i]="Ferreteria del barrio";
+					}
+				}
+				System.out.println("Ahora, se clasificaron los precios según el mas economico: ");
+				
+				double multiFinal=0;
+				double finalPayment=0;
+				
+				for(int i=0;i<totalMaterial;i++){
+					System.out.println("El material "+material[i]+" es mas economico en: ");
+					System.out.println(bestStore[i]+": "+bestPrice[i]);
+					
+					multiFinal=bestPrice[i]*amountProduct[i];
+					finalPayment=finalPayment+multiFinal;
+				}
+				
+				System.out.println("De la forma mas economica, el total a pagar sería (sin domicilio): "+finalPayment+totalWork);
 				
 		}
 }
